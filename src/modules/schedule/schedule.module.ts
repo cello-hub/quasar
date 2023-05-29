7e56c675d60ee3109ce09ea7cedb03c4576196f3
benson <gwanbit@gmail.com>
2023-05-29 17:39:32 +0800

    run task by env

--- src/modules/schedule/schedule.module.ts
+++ src/modules/schedule/schedule.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common'
 import { MysqlBackupService } from './mysql-backup.service'
+import { ENV_PROD } from 'src/utils/env'
 
+// 生产环境启动的定时任务
+const PROD_TASK_SERVICES = [MysqlBackupService]
+
+// 开发环境启动的定时任务(用于测试)
+const DEV_TASK_SERVICES = []
 @Module({
-  providers: [MysqlBackupService]
+  providers: ENV_PROD ? PROD_TASK_SERVICES : DEV_TASK_SERVICES
 })
 export class ScheduleModule {}
